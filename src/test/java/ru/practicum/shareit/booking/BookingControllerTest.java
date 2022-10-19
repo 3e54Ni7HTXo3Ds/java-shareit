31de0e70b410dfb1c2ba0b983ff511a9f426c12f
Maxim Pisarev <mspisarev@yandex.ru>
2022-10-19 07:02:47 +0300

    feat: спринт 15, дописываем тесты

---
+++ src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+
+class BookingControllerTest {
+}
+
